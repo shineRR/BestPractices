402e52153d29bbc1fec020e16e632fb9a1b1afb2
Ilya Baryko <38408304+shineRR@users.noreply.github.com>
2021-03-03 16:02:37 +0300

    Added loadMoreData function

--- PeoplePractice/PeoplePractice/View Controllers/Data View Controller/DataViewController.swift
+++ PeoplePractice/PeoplePractice/View Controllers/Data View Controller/DataViewController.swift
@@ -15,6 +15,7 @@ class DataViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private var entireModel: ApiData?
     private var listModels: [BaseModel] = []
     private var sectionName: String?
     var generalUrls: GeneralData?
@@ -28,17 +29,18 @@ class DataViewController: UIViewController {
         tableView.dataSource = self
         tableView.register(DataTableViewCell.self, forCellReuseIdentifier: "myCell")
         navigationController?.navigationBar.isHidden = false
-        fetchData()
+        fetchData(url: generalUrls?.people ?? "")
     }
     
     //  MARK: - Functions
     
-    private func fetchData() {
-        ApiHelper.parseApi(url: generalUrls?.people ?? "", of: PeopleData.self, name: "People", onSucess: { [weak self] obj in
+    private func fetchData(url: String) {
+        ApiHelper.parseApi(url: url, of: PeopleData.self, name: "People", onSucess: { [weak self] obj in
             if let model = obj as? PeopleData,
                let results = model.results {
+                self?.entireModel = obj
                 self?.sectionName = obj.object?.rawValue
-                self?.listModels = results
+                self?.listModels += results
             }
             self?.tableView.reloadData()
         })
@@ -57,9 +59,24 @@ extension DataViewController: UITableViewDelegate, UITableViewDataSource {
         return listModels.count
     }
     
+    func loadMoreData() {
+        guard let url = entireModel?.next else {
+            return
+        }
+
+        DispatchQueue.main.async {
+            self.fetchData(url: url)
+        }
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == (listModels.count - 1) {
+            loadMoreData()
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! DataTableViewCell
-                
+        
         cell.setupCell(modelName: listModels[indexPath.row].name)
         return cell
     }
